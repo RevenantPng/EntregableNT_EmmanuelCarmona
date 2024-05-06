2e54c0bd3cb3d38302343b48c19e0480aaeaaead
RevenantPng <163357939+RevenantPng@users.noreply.github.com>
2024-05-06 18:05:11 -0500

    Add files via upload

---
+++ entregable.py
@@ -0,0 +1,26 @@
+id = "SKU7702001044726"
+print(f"id: {id}")
+
+name = "Avena Original"
+name = input("Nombre del producto: ")
+print(f"Nombre: {name}")
+
+Description = " La leche de avena tiene una textura cremosa y un sabor similar al de la avena y se fabrica en varios sabores, como el azucarado, el no azucarado, de vainilla o chocolate."
+print(f"Descripcion del producto: {Description}")
+
+Cost = 8800
+
+print(f"Su costo es de: {Cost}")
+
+amount = 1
+amount = input("Ingrese la cantidad del producto: ")
+print(f"Cantidad de el producto: {amount}")
+
+state = "Con la ayuda de Dios, liquido y en buen estado"
+state = input("¿En que estado desea encontrar el producto? :")
+print(f"Estado del producto: {state}")
+
+Price = Cost/(1 - 0.3)
+
+print(f" La {name} tiene un precio de: {Price}")
+

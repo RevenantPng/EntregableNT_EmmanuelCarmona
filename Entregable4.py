673ec20b5a00f9059d1b4e9019268c9f15b60730
RevenantPng <163357939+RevenantPng@users.noreply.github.com>
2024-05-20 17:55:27 -0500

    Add files via upload

---
+++ Entregable4.py
@@ -0,0 +1,119 @@
+productos = []
+producto1 = []
+
+campos = 5
+
+i = 0
+
+while i < campos:
+    valor = input("Ingrese campo", (i +1))
+    producto1.append(valor)
+    i+=1
+    producto1.append(producto1)
+for item in producto1:
+    print(item)
+
+encabezados = ("Id", "Nombre", "Precio", "Cantidad", "Categoria")
+
+for encabezado in encabezados:
+    print(encabezado, end="\t")
+print()
+for item in producto1:
+    print(item, end="\t")
+print()
+
+
+producto2 = []
+
+campos = 5
+
+i = 0
+
+while i < campos:
+    valor = input("Ingrese campo", (i +1))
+    producto2.append(valor)
+    i+=1
+
+for item in producto2:
+    print(item)
+
+encabezados = ("Id", "Nombre", "Precio", "Cantidad", "Categoria")
+
+for encabezado in encabezados:
+    print(encabezado, end="\t")
+print()
+for item in producto2:
+    print(item, end="\t")
+print()
+
+
+producto3 = []
+
+campos = 5
+
+i = 0
+
+while i < campos:
+    valor = input("Ingrese campo", (i +1))
+    producto3.append(valor)
+    i+=1
+
+for item in producto3:
+    print(item)
+
+encabezados = ("Id", "Nombre", "Precio", "Cantidad", "Categoria")
+
+for encabezado in encabezados:
+    print(encabezado, end="\t")
+print()
+for item in producto3:
+    print(item, end="\t")
+print()
+
+
+producto4 = []
+
+campos = 5
+
+i = 0
+
+while i < campos:
+    valor = input("Ingrese campo", (i +1))
+    producto4.append(valor)
+    i+=1
+
+for item in producto4:
+    print(item)
+
+encabezados = ("Id", "Nombre", "Precio", "Cantidad", "Categoria")
+
+for encabezado in encabezados:
+    print(encabezado, end="\t")
+print()
+for item in producto4:
+    print(item, end="\t")
+print()
+
+
+producto5 = []
+
+campos = 5
+
+i = 0
+
+while i < campos:
+    valor = input("Ingrese campo", (i +1))
+    producto5.append(valor)
+    i+=1
+
+for item in producto5:
+    print(item)
+
+encabezados = ("Id", "Nombre", "Precio", "Cantidad", "Categoria")
+
+for encabezado in encabezados:
+    print(encabezado, end="\t")
+print()
+for item in producto5:
+    print(item, end="\t")
+print()
